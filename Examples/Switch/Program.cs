e7dca16d864dd5f076a52c64f4c9ff0baead994d
Albertinoz <ing.alberto.03@gmail.com>
2023-10-12 17:38:36 -0600

    Se concluye switch

--- Examples/Switch/Program.cs
+++ Examples/Switch/Program.cs
@@ -13,6 +13,13 @@ namespace Switch
                 case 1:
                     Console.WriteLine("Seleccionaste la opción uno");
                     break;
+
+                case 2:
+                    Console.WriteLine("Seleccionaste la opción uno");
+                    break;
+                //case < 0:
+                //    Console.WriteLine("Valor fuera de rango");
+                //    break;
                 default:
                     Console.WriteLine("Ninguna opción elegida");
                     break;
