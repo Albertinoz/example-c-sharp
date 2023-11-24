3201a3fdb5c32a19b3d85ee3781b84ccc58a49a5
Albertinoz <ing.alberto.03@gmail.com>
2023-11-24 17:09:52 -0600

    Se agrega ejemplo de Mutex para c#

---
+++ Examples/MutexEjemplo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace MutexEjemplo
+{
+    internal class Program
+    {
+        private static Mutex mutex = new Mutex();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = new Thread(DoWork);
+                thread.Start();
+            }
+
+            Console.ReadLine();
+        }
+
+        static void DoWork()
+        {
+            Console.WriteLine($"Hilo {Thread.CurrentThread.ManagedThreadId}");
+
+            mutex.WaitOne();
+
+            Console.WriteLine($"Hilo {Thread.CurrentThread.ManagedThreadId}");
+
+            mutex.ReleaseMutex();
+
+            Console.WriteLine($"Hilo {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
