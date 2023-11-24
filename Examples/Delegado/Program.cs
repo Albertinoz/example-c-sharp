34a07fa99defe2849100f77d3d1a56f83f1cf99f
Albertinoz <ing.alberto.03@gmail.com>
2023-11-24 17:18:06 -0600

    Ejemplo de un delegate.

---
+++ Examples/Delegado/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Delegado
+{
+    //Declaración de un delegate
+    public delegate void MiDelegate(string mensaje);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instancia del delegate
+            MiDelegate delegado = new MiDelegate(MiMetodo);
+
+            //Invocación del delegate
+            delegado("Hola desde el delegate");
+
+            //Invocación del delegate por inferencia de tipos
+            MiDelegate otroDelegado = MiMetodo;
+            otroDelegado("Saludos desde otro Delegate");
+        }
+
+        //Método que coincide con la firma del Delegate
+        static void MiMetodo(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+    }
+}
