5aefdf91f8e6dfc0656f26840bc01284029c7594
Albertinoz <ing.alberto.03@gmail.com>
2023-11-24 18:08:20 -0600

    Se agrega un ejemplo de Assert en C#

---
+++ Examples/AssertEjem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AssertEjem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado = Sumar(7, 3);
+
+            //Verifica que el resultado sea igual a 5
+            Debug.Assert(resultado == 5, "La suma no es igual a 5");
+
+            Console.WriteLine("Continúa...");
+        }
+
+        static int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
